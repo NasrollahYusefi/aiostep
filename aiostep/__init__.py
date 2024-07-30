3773d29bfd1e774d824f64021915442238ddf6b7
NasrollahYusefi <nasrollahyusefi2004@gmail.com>
2024-07-31 02:12:07 +0330

    Update __init__.py

--- aiostep/__init__.py
+++ aiostep/__init__.py
@@ -1,1 +1,24 @@
+"""
+A Python library to handle steps in aiogram framework.
+"""
+__author__ = "Nasrollah Yusefi"
+__version__ = "0.1.0"
 
+__all__ = [
+    "Listen",
+    "change_root_store",
+    "register_next_step",
+    "unregister_steps",
+    "wait_for",
+    "clear",
+]
+
+from .steps import (
+    MetaStore as MetaStore,
+    Listen as Listen,
+    change_root_store as change_root_store,
+    register_next_step as register_next_step,
+    unregister_steps as unregister_steps,
+    wait_for as wait_for,
+    clear as clear
+)
